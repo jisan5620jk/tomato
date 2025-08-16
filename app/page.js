a97458dacc4c941b1a1bcf9ad8e15272739c79b6
jisan5620jk <jisank5620@gmail.com>
2025-08-16 12:06:21 +0600

    Some Issue Solve

--- app/page.js
+++ app/page.js
@@ -16,29 +16,31 @@ export default function Home() {
           />
         </div>
         <div className=" text-center mt-[52px]">
-          <h1 className="text-[56px] font-normal [font-family:var(--font-open-sans)]">
-            tomato.com
-          </h1>
-          <h2 className="text-3xl font-light [font-family:var(--font-open-sans)] -mt-2 mb-11">
+          <h1 className="text-[56px] font-normal font-open-sans">tomato.com</h1>
+          <h2 className="text-3xl font-light font-open-sans -mt-2 mb-11">
             is a totally awesome idea still being worked on.
           </h2>
-          <p className="text-3xl font-light [font-family:var(--font-open-sans)]">
+          <p className="text-3xl font-light font-open-sans">
             Check back later.
           </p>
         </div>
 
         <form
-          action="https://www.hover.com/domains/results"
+          action="#"
           className="mt-[82px] mb-[30px] mx-auto relative inline-block max-w-[460px] w-full"
           method="get"
         >
           <input
-            name="serch"
+            name="search"
             placeholder="Find a domain for your own great idea."
             type="text"
-            className="w-[460px] h-10 px-[18px] py-2 [font-family:var(--font-open-sans)] text-[#283039] border border-[#d4d6d8] focus:outline-none"
-          ></input>
-          <button className="absolute top-0 right-0 size-10 flex justify-center items-center bg-[#3ab795] text-white border border-[#d4d6d8]">
+            className="w-[460px] h-10 px-[18px] py-2 font-open text-[#283039] border border-[#d4d6d8] focus:outline-none"
+          />
+
+          <button
+            aria-label="Search"
+            className="absolute top-0 right-0 size-10 flex justify-center items-center bg-[#3ab795] text-white border border-[#d4d6d8]"
+          >
             <FaSearch />
           </button>
         </form>
@@ -47,8 +49,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://www.hover.com/?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 Home
               </a>
@@ -56,8 +58,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://www.hover.com/transfer_in?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 Transfer
               </a>
@@ -65,8 +67,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://www.hover.com/renew?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 Renew
               </a>
@@ -74,8 +76,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://www.hover.com/domain_pricing?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 Domain Pricing
               </a>
@@ -83,8 +85,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://www.hover.com/email?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 Email
               </a>
@@ -92,8 +94,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://www.hover.com/about?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 About Us
               </a>
@@ -101,8 +103,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://help.hover.com/home?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 Help
               </a>
@@ -110,8 +112,8 @@ export default function Home() {
             <li>
               <a
                 rel="nofollow"
-                href="https://www.hover.com/tools?source=parked"
-                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] [font-family:var(--font-open-sans)] text-[11px] uppercase"
+                href="#"
+                className="text-[#3ab795] transition-all duration-500 hover:text-[#283039] font-open-sans text-[11px] uppercase"
               >
                 Your Account
               </a>
@@ -123,6 +125,7 @@ export default function Home() {
             <li>
               <a
                 href="#"
+                aria-label="Facebook"
                 className="size-7 flex items-center justify-center rounded-full bg-[#3ab795] text-white"
               >
                 <FaFacebookF />
@@ -131,6 +134,7 @@ export default function Home() {
             <li>
               <a
                 href="#"
+                aria-label="Twitter"
                 className="size-7 flex items-center justify-center rounded-full bg-[#3ab795] text-white"
               >
                 <FaXTwitter />
@@ -139,6 +143,7 @@ export default function Home() {
             <li>
               <a
                 href="#"
+                aria-label="Instagram"
                 className="size-7 flex items-center justify-center rounded-full bg-[#3ab795] text-white"
               >
                 <FaInstagram />
@@ -150,7 +155,7 @@ export default function Home() {
       <footer className="mt-[74px] border-t border-[#d4d6d8] py-7">
         <ul className="flex items-center justify-center gap-x-8 gap-y-2 flex-wrap">
           <li>
-            <p className="[font-family:var(--font-open-sans)] text-sm">
+            <p className="font-open-sans text-sm">
               Copyright &copy; {new Date().getFullYear()} Hover
             </p>
           </li>
@@ -158,8 +163,8 @@ export default function Home() {
           <li>
             <a
               rel="nofollow"
-              href="https://www.hover.com/tos?source=parked"
-              className="[font-family:var(--font-open-sans)] text-sm text-[#3ab795] transition-all duration-500 hover:"
+              href="#"
+              className="font-open-sans text-sm text-[#3ab795] transition-all duration-500 hover:text-[#283039]"
             >
               Terms of Service
             </a>
@@ -167,8 +172,8 @@ export default function Home() {
           <li>
             <a
               rel="nofollow"
-              href="https://www.hover.com/privacy?source=parked"
-              className="[font-family:var(--font-open-sans)] text-sm text-[#3ab795] transition-all duration-500 hover:text-[#283039]"
+              href="#"
+              className="font-open-sans text-sm text-[#3ab795] transition-all duration-500 hover:text-[#283039]"
             >
               Privacy
             </a>
